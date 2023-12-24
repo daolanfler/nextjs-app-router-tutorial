3ed876476084934b1c9434528712e1d078593a97
daolanfler <daolanfler@gmail.com>
2023-12-24 15:53:56 +0800

    ch11

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,55 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default function Page() {
-  return <p>Invoices Page</p>;
+/**
+ * Your search functionality will span the client and the server.
+ * When a user searches for an invoice on the client, the URL params
+ * will be updated, data will be fetched on the server, and the
+ * table will re-render on the server with the new data.
+ *
+ * Why we use search query?
+ * - Server-Side Rendering and Initial Load: URL parameters can be directly
+ * consumed on the server to render the initial state, making it easider to
+ * handler server rendering.
+ * - Analytics and Tracking: Having search queries and filters directly in the
+ * URL makes it easier to track user behaviour without requiring additional
+ * client-side logic.
+ */
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
